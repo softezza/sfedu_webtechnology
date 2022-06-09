b45da66f5154a760766110414794771a5ee98d34
chin-chopa-chin-chopa <semen.9056@mail.ru>
2022-06-09 22:34:38 +0300

    img to pdf

--- Petrov/project/main.py
+++ Petrov/project/main.py
@@ -4,11 +4,11 @@ from io import BytesIO
 from typing import List
 
 from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.openapi.models import Response
 from fastapi.templating import Jinja2Templates
 
 from fpdf import FPDF
 from starlette.responses import FileResponse
+from PyPDF2 import PdfMerger
 
 from helpers import upload_file_to_bytes
 
@@ -37,10 +37,49 @@ async def create_upload_files(files: List[UploadFile]):
 
         rows = buffer.getvalue().decode().split('\n')
         for r in rows:
-            pdf.cell(200, 20, txt=r, ln=1, align='C')
+            pdf.cell(200, 20, txt=r, ln=1, align='R')
 
         fn = f'media/{files[0].filename}__.pdf'
         pdf.output(fn)
+        return FileResponse(fn, media_type='application/pdf', filename=files[0].filename + '__.pdf')
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.post("/img_to_pdf/")
+async def create_upload_files(files: List[UploadFile]):
+    try:
+        tmp_img_name = f'media/tmp__{files[0].filename}'
+        with open(tmp_img_name, 'wb') as fp:
+            fp.write(await files[0].read())
+
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.image(tmp_img_name, 10, 10)
+
+        fn = f'media/{files[0].filename}__.pdf'
+        pdf.output(fn)
+
+        os.remove(tmp_img_name)
+        return FileResponse(fn, media_type='application/pdf', filename=files[0].filename + '__.pdf')
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.post("/merge_pdf/")
+async def create_upload_files(files: List[UploadFile]):
+    try:
+        tmp_img_name = f'media/tmp__{files[0].filename}'
+        with open(tmp_img_name, 'wb') as fp:
+            fp.write(await files[0].read())
+
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.image(tmp_img_name, 10, 10)
+
+        fn = f'media/{files[0].filename}__.pdf'
+        pdf.output(fn)
+
+        os.remove(tmp_img_name)
         return FileResponse(fn, media_type='application/pdf', filename=files[0].filename + '__.pdf')
     except Exception as e:
         return {"error": str(e)}
